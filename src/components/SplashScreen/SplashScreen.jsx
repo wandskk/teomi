becd194e236824026c67065a0bb478a7682eb844
Wanderson Kenedy <wandersonkenedy13@gmail.com>
2023-11-27 11:13:50 -0300

    ajustando

--- src/components/SplashScreen/SplashScreen.jsx
+++ src/components/SplashScreen/SplashScreen.jsx
@@ -1,27 +1,28 @@
-'use client';
+"use client";
 
-import React from 'react';
-import splashScreenImage from '@/assets/images/splashScreen.svg';
-import Image from 'next/image';
-import { UserContext } from '@/context/UserContext';
-import { CSSTransition } from 'react-transition-group';
-import '@/styles/SplashScreen/SplashScreen.scss';
+import React from "react";
+import splashScreenImage from "@/assets/images/splashScreen.svg";
+import Image from "next/image";
+import { UserContext } from "@/context/UserContext";
+import { CSSTransition } from "react-transition-group";
+import "@/styles/SplashScreen/SplashScreen.scss";
 
 const SplashScreen = () => {
   const { splashScreen } = React.useContext(UserContext);
 
-  return (
-    <CSSTransition
-      in={splashScreen}
-      timeout={300}
-      classNames='fade'
-      unmountOnExit
-    >
-      <div className='splashScreen fade-content'>
-        <Image priority={true} src={splashScreenImage} alt='Splash Screen' />
-      </div>
-    </CSSTransition>
-  );
+  if (splashScreen)
+    return (
+      <CSSTransition
+        in={splashScreen}
+        timeout={300}
+        classNames="fade"
+        unmountOnExit
+      >
+        <div className="splashScreen fade-content">
+          <Image priority={true} src={splashScreenImage} alt="Splash Screen" />
+        </div>
+      </CSSTransition>
+    );
 };
 
 export default SplashScreen;
